d48e25ec992784fe4cc6edbb4cf3a71e16ff79e0
Adam Scott <adam.scott@cfpb.gov>
2014-07-28 14:27:42 -0400

    calculate overall cost

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 /*
  * overall-loan-cost
+ * calculate the overall cost of a loan over its life or a given period
+ * @param {object} opts
+ * opts = {
+ *    amount: num,
+ *    rate: num,
+ *    totalTerm: num,
+ *    amortizeTerm: num,
+ *    downPayment: num,
+ *    closingCosts: num
+ * }
+ * @returns number
  */
 
 'use strict';
 
-exports.awesome = function() {
-  return 'awesome';
+var amortize = require('amortize');
+
+module.exports = function(opts) {
+  var amortizedVal,
+      totalCost,
+      totalEquity,
+      overallCost;
+
+  amortizedVal = amortize({
+    amount: opts.amount,
+    rate: opts.rate,
+    totalTerm: opts.totalTerm,
+    amortizeTerm: opts.amortizeTerm
+  });
+
+  totalCost = Number(opts.closingCosts) + Number(amortizedVal.interest);
+  totalEquity = Number(opts.downPayment) + Number(amortizedVal.principal);
+  overallCost = totalCost + totalEquity;
+
+  return overallCost;
+
 };
