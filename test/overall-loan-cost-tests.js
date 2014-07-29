b69e463cc7698ac89f5dfee0de4b11fa78903d36
Adam Scott <adam.scott@cfpb.gov>
2014-07-29 14:28:24 -0400

    first round of tests

--- test/overall-loan-cost-tests.js
+++ test/overall-loan-cost-tests.js
@@ -29,11 +29,158 @@ exports.overallLoanCost = {
   },
   'Amount, rate, and term': function(test) {
     test.expect(1);
-    // tests here
     test.equal(
       cost({amountBorrowed: 300000, rate: 4.25, totalTerm: 360}).overallCost,
       531295.08,
       'should be 531295.08.'
+    );
+    test.done();
+  },
+  '$200k values': function(test) {
+    test.expect(4);
+    test.deepEqual(
+      cost({
+        amountBorrowed: 180000,
+        rate: 3.5,
+        totalTerm: 360,
+        downPayment: 20000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 113980.96,
+        totalEquity: 200000,
+        overallCost: 313980.96
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.deepEqual(
+      cost({
+        amountBorrowed: 180000,
+        rate: 3,
+        totalTerm: 360,
+        downPayment: 20000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 96199.41,
+        totalEquity: 200000,
+        overallCost: 296199.41
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.deepEqual(
+      cost({
+        amountBorrowed: 180000,
+        rate: 3.5,
+        totalTerm: 180,
+        downPayment: 20000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 54621.94,
+        totalEquity: 200000,
+        overallCost: 254621.94
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.deepEqual(
+      cost({
+        amountBorrowed: 160000,
+        rate: 3.5,
+        totalTerm: 360,
+        downPayment: 40000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 101649.74,
+        totalEquity: 200000,
+        overallCost: 301649.74
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.done();
+  },
+  'Various values': function(test) {
+    test.expect(4);
+    test.deepEqual(
+      cost({
+        amountBorrowed: 255000,
+        rate: 4.5,
+        totalTerm: 360,
+        downPayment: 45000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 213137.11,
+        totalEquity: 300000,
+        overallCost: 513137.11
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.deepEqual(
+      cost({
+        amountBorrowed: 130000,
+        rate: 5,
+        totalTerm: 360,
+        downPayment: 20000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 124232.52,
+        totalEquity: 150000,
+        overallCost: 274232.52
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.deepEqual(
+      cost({
+        amountBorrowed: 210000,
+        rate: 4.125,
+        totalTerm: 180,
+        downPayment: 40000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 74975.75,
+        totalEquity: 250000,
+        overallCost: 324975.75
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.deepEqual(
+      cost({
+        amountBorrowed: 390000,
+        rate: 3.25,
+        totalTerm: 360,
+        downPayment: 60000,
+        closingCosts: 3000
+      }),
+      {
+        totalCost: 224029.67,
+        totalEquity: 450000,
+        overallCost: 674029.67
+      },
+      'should return the correct total cost, total equity, and overall cost'
+    );
+    test.done();
+  },
+  'Various time horizons': function(test) {
+    test.expect(1);
+    test.deepEqual(
+      cost({
+        amountBorrowed: 255000,
+        rate: 4.5,
+        totalTerm: 360,
+        amortizeTerm: 84,
+        downPayment: 45000,
+        closingCosts: 6000
+      }),
+      {
+        totalCost: 81449.02,
+        totalEquity: 78082.98,
+        overallCost: 159532
+      },
+      'should return the correct total cost, total equity, and overall cost after 7 years'
     );
     test.done();
   }
